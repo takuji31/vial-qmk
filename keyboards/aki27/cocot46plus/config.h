e5351a3255577f8bd339e436fb11846097249920
Takuji Nishibayashi <takuji31@users.noreply.github.com>
2024-10-06 01:42:40 +0900

    Add more Layers

--- keyboards/aki27/cocot46plus/config.h
+++ keyboards/aki27/cocot46plus/config.h
@@ -34,3 +34,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP2
 #define I2C1_SCL_PIN GP3
+
+// VIA
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#undef LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#undef LOCKING_RESYNC_ENABLE
