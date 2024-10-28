030d7c072556adfaf4ca9e98ba704290244efeef
Takuji Nishibayashi <takuji31@users.noreply.github.com>
2024-10-29 01:36:37 +0900

    Add vial keymap for 7sKB

---
+++ keyboards/salicylic_acid3/7skb/keymaps/vial/config.h
@@ -0,0 +1,26 @@
+/* Copyright 2018 Salicylic_acid3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* Select hand configuration */
+
+#define VIAL_KEYBOARD_UID {0x6E, 0x56, 0xFD, 0x9F, 0xBE, 0x33, 0xB1, 0x88}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 180
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
