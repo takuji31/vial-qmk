6ecd5613bb54b04d23f58f5c55e57f92628369ee
Takuji Nishibayashi <takuji31@gmail.com>
2025-09-01 10:10:29 +0900

    rename constant ref a7bf8e6

--- keyboards/geek_rabb1t/trackpad/gr_trackpad65_driver.h
+++ keyboards/geek_rabb1t/trackpad/gr_trackpad65_driver.h
@@ -18,7 +18,7 @@
 #include "report.h"
 #include "pointing_device.h"
 
-#define CONSTRAIN_HID_HV(amt) ((amt) < HV_REPORT_MIN ? HV_REPORT_MIN : ((amt) > HV_REPORT_MAX ? HV_REPORT_MAX : (amt)))
+#define CONSTRAIN_HID_HV(amt) ((amt) < MOUSE_REPORT_HV_MIN ? MOUSE_REPORT_HV_MIN : ((amt) > MOUSE_REPORT_HV_MAX ? MOUSE_REPORT_HV_MAX : (amt)))
 
 typedef enum  {
     trackpad_event_none = -1,
